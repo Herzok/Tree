82dd538c24e16eacf95216be9ac9a0bf40925d0b
Artem <yarsubmariners@gmail.com>
2019-08-21 16:14:19 +0300

    добавлен тест на отступы, буфер в 1 функции.

--- TreeTest.php
+++ TreeTest.php
@@ -16,12 +16,177 @@ class TreeTest extends TestCase
      * @return array
      * @throws ReflectionException
      */
-    public function createReflectionMethod(string $class, string $method, bool $isShowFile)
+    private function createReflectionMethod(string $class, string $method, bool $isShowFile)
     {
         $refMethod = new ReflectionMethod($class, $method);
         $tree = new Tree('tree', $isShowFile);
         $refMethod->setAccessible(true);
         return [$tree, $refMethod];
+    }
+
+    /**
+     * Возвращение отступов
+     *
+     * @return array
+     */
+    public function prefixes()
+    {
+        return [["\t", [true]],
+            ["\t│\t", [true, false]],
+            ["\t│\t\t", [true, false, true]]];
+    }
+
+    /**
+     * Че-то делается
+     */
+    private function getReturnResult(string $class, string $method, bool $isShowFile, array $params)
+    {
+        $resultCreateRefMethod = $this->createReflectionMethod($class, $method, $isShowFile);
+        ob_start();
+        $resultCreateRefMethod [1]->invokeArgs($resultCreateRefMethod [0], $params);
+        $actual = ob_get_clean();
+        return $actual;
+    }
+
+
+    /**
+     * Проверка правильности результата метода isLastDir
+     *
+     * @throws ReflectionException
+     */
+    public function testIsLastDir()
+    {
+        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'isLastDir', false);
+        $result = $resultCreateRefMethod [1]->invokeArgs($resultCreateRefMethod [0], [1, 1]);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Проверка вводимых параметров
+     */
+    public function testPathIsShowFiles()
+    {
+        $tree = new Tree('tree', true);
+        $this->assertTrue($tree->isShowFiles);
+        $this->assertNotNull($tree->path);
+    }
+
+    /**
+     * Проверка вывода пустого файла
+     *
+     * @throws ReflectionException
+     */
+    public function testEmptyPrintFile()
+    {
+        $actual = $this->getReturnResult(
+            'Tree',
+            'printFiles',
+            true,
+            ['tree/data/empty.txt', 'empty.txt', 0, 1, []]
+        );
+        $expected = "├── empty.txt ( empty )\n";
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Тест отступов
+     *
+     * @dataProvider prefixes
+     * @throws ReflectionException
+     */
+    public function testPrefixesPrintFile(string $prefix, array $levelDirs)
+    {
+        $actual = $this->getReturnResult(
+            'Tree',
+            'printFiles',
+            true,
+            ['tree/data/empty.txt', 'empty.txt', 0, 1, $levelDirs]
+        );
+        $expected = $prefix;
+        $this->assertContains($expected, $actual);
+    }
+
+    /**
+     * Проверка вывода не пустого результата метода filterResultScanDir
+     *
+     * @throws ReflectionException
+     */
+    public function testResultScanDir()
+    {
+        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'filterResultScanDir', false);
+        $result = $resultCreateRefMethod [1]->invokeArgs($resultCreateRefMethod [0], ['tree']);
+        $this->assertNotNull($result);
+    }
+
+    /**
+     * Проверка написания последней директории
+     *
+     * @throws ReflectionException
+     */
+    public function testPrintLastDir()
+    {
+        $actual = $this->getReturnResult(
+            'Tree',
+            'printFiles',
+            false,
+            ['tree', 'data', 0, 0, []]
+        );
+        $expected = "└── data\n";
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Проверка написания не последней тиректории
+     *
+     * @throws ReflectionException
+     */
+    public function testPrintDir()
+    {
+        $actual = $this->getReturnResult(
+            'Tree',
+            'printFiles',
+            false,
+            ['tree', 'data', 0, 1, []]
+        );
+        $expected = "├── data\n";
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Проверка вывода размера файла
+     *
+     * @throws ReflectionException
+     */
+    public function testSizePrintFile()
+    {
+        $actual = $this->getReturnResult(
+            'Tree',
+            'printFiles',
+            true,
+            ['tree/README.md', 'README.md', 0, 1, []]
+        );
+        $expected = "├── README.md (2073 bytes)\n";
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Проверка дерева каталогов
+     */
+    public function testShowDirsTree()
+    {
+        $expected = "└── data
+\t├── dist
+\t│\t├── css
+\t│\t├── html
+\t│\t└── js
+\t└── src
+\t\t└── vue\n";
+        $tree = new Tree('tree', false);
+        ob_start();
+        $tree->show();
+        $actual = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals($expected, $actual);
     }
 
     /**
@@ -49,135 +214,5 @@ class TreeTest extends TestCase
         $actual = ob_get_contents();
         ob_end_clean();
         $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка дерева каталогов
-     */
-    public function testShowDirsTree()
-    {
-        $expected = "└── data
-\t├── dist
-\t│\t├── css
-\t│\t├── html
-\t│\t└── js
-\t└── src
-\t\t└── vue\n";
-        $tree = new Tree('tree', false);
-        ob_start();
-        $tree->show();
-        $actual = ob_get_contents();
-        ob_end_clean();
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка написания последней директории
-     *
-     * @throws ReflectionException
-     */
-    public function testPrintLastDir()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'printFiles', false);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        ob_start();
-        $refMethod->invokeArgs($modelTree, ['tree', 'data', 0, 0, []]);
-        $actual = ob_get_contents();
-        ob_end_clean();
-        $expected = "└── data\n";
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка написания не последней тиректории
-     *
-     * @throws ReflectionException
-     */
-    public function testPrintDir()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'printFiles', false);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        ob_start();
-        $refMethod->invokeArgs($modelTree, ['tree', 'data', 0 , 1, []]);
-        $actual = ob_get_contents();
-        ob_end_clean();
-        $expected = "├── data\n";
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка вывода размера файла
-     *
-     * @throws ReflectionException
-     */
-    public function testSizePrintFile()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'printFiles', true);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        ob_start();
-        $refMethod->invokeArgs($modelTree, ['tree/README.md', 'README.md', 0, 1, []]);
-        $actual = ob_get_contents();
-        ob_end_clean();
-        $expected = "├── README.md (2073 bytes)\n";
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка вывода пустого файла
-     *
-     * @throws ReflectionException
-     */
-    public function testEmptyPrintFile()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'printFiles', true);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        ob_start();
-        $refMethod->invokeArgs($modelTree, ['tree/data/empty.txt', 'empty.txt', 1, 2, [true]]);
-        $actual = ob_get_contents();
-        ob_end_clean();
-        $expected = "\t├── empty.txt ( empty )\n";
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Проверка вводимых параметров
-     */
-    public function testPathIsShowFiles()
-    {
-        $tree = new Tree('tree', true);
-        $this->assertTrue($tree->isShowFiles);
-        $this->assertNotNull($tree->path);
-    }
-
-    /**
-     * Проверка правильности результата метода isLastDir
-     *
-     * @throws ReflectionException
-     */
-    public function testIsLastDir()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'isLastDir', false);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        $result = $refMethod->invokeArgs($modelTree, [1, 1]);
-        $this->assertTrue($result);
-    }
-
-    /**
-     * Проверка вывода не пустого результата метода filterResultScanDir
-     *
-     * @throws ReflectionException
-     */
-    public function testResultScanDir()
-    {
-        $resultCreateRefMethod = $this->createReflectionMethod('Tree', 'filterResultScanDir', false);
-        $modelTree = $resultCreateRefMethod [0];
-        $refMethod = $resultCreateRefMethod [1];
-        $result = $refMethod->invokeArgs($modelTree, ['tree']);
-        $this->assertNotNull($result);
     }
 }
