0f3c1c9005b491eb247deae0d083e5c2cd685919
Artem <yarsubmariners@gmail.com>
2019-08-21 10:05:27 +0300

    тесты дир. и файлов.

--- TreeTest.php
+++ TreeTest.php
@@ -8,7 +8,20 @@ class TreeTest extends TestCase
 {
     public function testShowFullTree()
     {
-    	$expected = "111111";
+    	$expected = "├── README.md (2073 bytes)
+└── data
+\t├── dist
+\t│\t├── css
+\t│\t│\t└── app.css (14 bytes)
+\t│\t├── html
+\t│\t│\t└── index.html (15 bytes)
+\t│\t└── js
+\t│\t\t└── app.js (13 bytes)
+\t├── empty.txt ( empty )
+\t└── src
+\t\t├── vue
+\t\t│\t└── main.js (20 bytes)
+\t\t└── zzz.txt (21 bytes)\n";
 		$tree = new Tree('tree', true);
     	ob_start();
         $tree->show();
@@ -18,7 +31,13 @@ class TreeTest extends TestCase
     }
     public function testShowDirsTree()
 	{
-		$expected = "";
+		$expected = "└── data
+\t├── dist
+\t│\t├── css
+\t│\t├── html
+\t│\t└── js
+\t└── src
+\t\t└── vue\n";
 		$tree = new Tree('tree', false);
 		ob_start();
 		$tree->show();
